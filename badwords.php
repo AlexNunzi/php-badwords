f54ef5e9babdd38e6eb5b8c8cb5b66975ca92cfa
Alex Nunzi <alex.nunzi@hotmail.it>
2023-04-17 16:42:55 +0200

    Invio tramite metodo GET dei dati tramite il form all'interno di index.html al file badwords.php che restituisce il codice html contenente la frase inserita con la sua lunghezza prima della censura e di seguito la frase censurata con la nuova lunghezza

---
+++ badwords.php
@@ -0,0 +1,25 @@
+<?php
+$paragraph = $_GET['paragraph'];
+$keyword = $_GET['keyword'];
+
+$censoredParagraph = str_replace(trim($keyword), "***", $paragraph);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Badwords</title>
+</head>
+<body>
+    <h2>La frase che hai inserito è la seguente ed è lunga <?php echo strlen(trim($paragraph))?> caratteri:</h2>
+    <p><?php echo trim($paragraph); ?></p>
+    <h2>La parola da censurare è:</h2>
+    <p><?php echo trim($keyword) ?></p>
+    <h2>La frase inserita e censurata è la seguente ed è lunga <?php echo strlen(trim($censoredParagraph))?> caratteri</h2>
+    <p><?php echo trim($censoredParagraph) ?> </p>
+</body>
+</html>
